dd1c0b7b098950a297b6ae597128d097fa29465e
Tanja Bunk <tabergma@gmail.com>
2025-08-26 12:45:43 +0200

    filter input to a2a agent

--- car-purchase-assistant/custom/car_shopping_agent.py
+++ car-purchase-assistant/custom/car_shopping_agent.py
@@ -1,11 +1,25 @@
 from rasa.agents.protocol.a2a.a2a_agent import A2AAgent
-from rasa.agents.schemas import AgentOutput
+from rasa.agents.schemas import AgentInput, AgentOutput
 
 from typing import List
+from rasa.agents.schemas.agent_input import AgentInputSlot
 from rasa.shared.core.events import SlotSet
 
 
 class CarShoppingAgent(A2AAgent):
+
+    async def process_input(self, input: AgentInput) -> AgentInput:
+        """Pre-process the input before sending it to Rasa."""
+        slots_to_keep: List[AgentInputSlot] = []
+        for slot in input.slots:
+            if slot.name == "recommended_car_models":
+                slots_to_keep.append(slot)
+            if slot.name == "recommended_car_details":
+                slots_to_keep.append(slot)
+
+        input.slots = slots_to_keep
+        return input
+
     async def process_output(self, output: AgentOutput) -> AgentOutput:
         """Post-process the output before returning it to Rasa."""
         tool_results = output.tool_results

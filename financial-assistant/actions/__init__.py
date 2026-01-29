166759c97e6e44ef594e5b3fe28285d7db2b222d
Tanja Bunk <tabergma@gmail.com>
2026-01-29 15:18:56 +0100

    add filter transactions

--- financial-assistant/actions/__init__.py
+++ financial-assistant/actions/__init__.py
@@ -2,6 +2,7 @@
 
 from actions.list_cards import ActionListCards
 from actions.lock_card import ActionLockCard
+from actions.list_transactions import ActionListTransactions
 from actions.pay_bill import ActionGetCardBalance, ActionPayBill
 from actions.report_lost_card import ActionLockReportedCard, ActionReportLostCard
 from actions.session_start import ActionSessionStart
@@ -14,4 +15,5 @@ __all__ = [
     "ActionReportLostCard",
     "ActionLockReportedCard",
     "ActionSessionStart",
+    "ActionListTransactions",
 ]
